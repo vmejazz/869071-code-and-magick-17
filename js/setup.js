d12de7fd42b08e78a230c7ad152687c835dac0ef
vmejazz <vmejazz@gmail.com>
2019-06-14 17:27:19 +0300

    Домашка #3

---
+++ js/setup.js
@@ -0,0 +1,89 @@
+// Файл setup.js
+'use strict';
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+var firstNames = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+
+var secondNames = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+
+var coatColors = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+
+var eyesColors = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
+var getRandomItem = function (array) {
+  return Math.floor(Math.random() * array.length);
+};
+
+var getWizardProperties = function (wizardCount) {
+  var arrayWizard = [];
+  var i = 1;
+  while (i <= wizardCount) {
+    var wizardPropetry = {
+      name: firstNames[getRandomItem(firstNames)] + ' ' + secondNames[getRandomItem(secondNames)],
+      coatColor: coatColors[getRandomItem(coatColors)],
+      eyesColor: eyesColors[getRandomItem(eyesColors)]
+    };
+    arrayWizard.push(wizardPropetry);
+    i++;
+  }
+  return arrayWizard;
+};
+
+var wizards = getWizardProperties(4);
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
